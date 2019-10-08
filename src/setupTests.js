d08ce5a9d6d41fb040d95f7ba5795c35697c4cc3
SketchBuch <sb.design.de@gmail.com>
2019-10-08 06:36:58 +0200

    linting updates

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,8 @@ import 'jest-localstorage-mock';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+console.log('####### setup');
+
 configure({ adapter: new Adapter() });
 
 window.reportr = {
